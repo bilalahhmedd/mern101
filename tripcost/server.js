c448933f13be12ddc1044b5f88e1ffd934a5bf77
bilalahhmedd <bilalahhmedd@gmail.com>
2022-06-08 18:18:53 +0500

    express app init, stubs added

---
+++ tripcost/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongo = require("mongodb").MongoClient
+
+const app  = express()
+
+// adding stubs now
+
+app.post("/trip", (req, res) => {
+    /* */
+  })
+  app.get("/trips", (req, res) => {
+    /* */
+  })
+  app.post("/expense", (req, res) => {
+    /* */
+  })
+  app.get("/expenses", (req, res) => {
+    /* */
+  })
